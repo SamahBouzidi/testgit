840a6073ba02a1bee62f45096b331bb4b3cf1359
samah <samah.bouzidi@gmail.com>
2022-07-03 23:44:53 +0200

    Ajout Pangolin.java et Modification de Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main (String args [])
     {
         System.out.println("Hello!");
+        Pangolin p = new Pangolin("toto", 5);
+        System.out.println(p.toString());
 
     }
 }
