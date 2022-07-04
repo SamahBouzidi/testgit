6dada55e4ba402a020f9592c2cd2c567ddfc0966
samah <samah.bouzidi@gmail.com>
2022-07-04 09:26:33 +0200

    Revert "Ajout de la modification à supprimer"
    This reverts commit d6001df424b06d06877cbbf81796a4bc6193ea26.

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main (String args [])
     {
-        System.out.println("Hello Pangolin!");
+        System.out.println("Hello!");
         Pangolin p = new Pangolin("toto", 5);
         System.out.println(p.toString());
 
