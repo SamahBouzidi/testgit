d6001df424b06d06877cbbf81796a4bc6193ea26
samah <samah.bouzidi@gmail.com>
2022-07-04 09:17:19 +0200

    Ajout de la modification à supprimer

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main (String args [])
     {
-        System.out.println("Hello!");
+        System.out.println("Hello Pangolin!");
         Pangolin p = new Pangolin("toto", 5);
         System.out.println(p.toString());
 
