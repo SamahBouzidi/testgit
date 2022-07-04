054d12f664122e22dc86541d11163ff480c1ed69
samah <samah.bouzidi@gmail.com>
2022-07-04 15:15:55 +0200

    new Pangolin bidule

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Hello!");
         Pangolin p = new Pangolin("toto", 5);
         Pangolin p1 = new Pangolin("Titi", 5);
+        Pangolin p2 = new Pangolin("Bidule", 5);
         System.out.println(p.toString());
         System.out.println(p1.crier());
         System.out.println(p.crier());
